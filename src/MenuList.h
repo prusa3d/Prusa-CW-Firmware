5b8479b2aa2bc6265031a54b693ff229fde1fb0b
Lubomír Bláha <tritol@trilogic.cz>
2020-02-27 10:34:58 +0100

    coding style only

--- src/MenuList.h
+++ src/MenuList.h
@@ -7,19 +7,17 @@
 #include "LiquidCrystal_Prusa.h"
 #include <stdint.h>
 
-struct Scrolling_item
-{
-   const char *caption_P;
-   bool visible;
-   LiquidCrystal_Prusa::Terminator last_symbol;
+struct Scrolling_item {
+	const char *caption_P;
+	bool visible;
+	LiquidCrystal_Prusa::Terminator last_symbol;
 };
 
 uint_least8_t scrolling_list_P(const Scrolling_item items[], uint8_t item_count);
 
 template<uint8_t n>
-uint_least8_t scrolling_list_P(const Scrolling_item (&items)[n])
-{
-  return scrolling_list_P(items, sizeof(items)/sizeof(Scrolling_item));
+uint_least8_t scrolling_list_P(const Scrolling_item (&items)[n]) {
+	return scrolling_list_P(items, sizeof(items)/sizeof(Scrolling_item));
 }
 
 void scrolling_list_set(uint8_t index);
