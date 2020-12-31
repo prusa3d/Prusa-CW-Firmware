050044684fc2e87c658dc84b37ecc72b499989f6
Saij <christoph@christophfriedrich.de>
2020-12-31 13:44:18 +0100

    Optimized build script to make builds on Windows easier

--- build.sh
+++ build.sh
@@ -2,12 +2,47 @@
 
 BUILD_ENV="1.0.1"
 ENV_DIR="build/env"
+SYSTEM="Linux64"
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+REDOWNLOAD="false"
+
+usage() {
+	echo "usage: build.sh --download --system WIN|LINUX"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --platform)           
+			shift
+            if [ "$1" == "WIN" ]; then
+            	SYSTEM="Win64"
+            fi
+            if [ "$1" == "LINUX" ]; then
+            	SYSTEM="Linux64"
+            fi
+            ;;
+
+        --download )    
+			REDOWNLOAD="true"
+            ;;
+
+        -h | --help )           
+			usage
+            exit
+            ;;
+    esac
+    shift
+done
+
+if [ "$REDOWNLOAD" == "true" ]; then
+	rm -rf "$ENV_DIR" || exit 6
+fi
 
 if [ ! -d "$ENV_DIR" ]; then
 	mkdir -p "$ENV_DIR" || exit 1
-    wget "https://github.com/prusa3d/MM-build-env/releases/download/$BUILD_ENV/MM-build-env-Linux64-$BUILD_ENV.zip" || exit 2
-    unzip -q "MM-build-env-Linux64-$BUILD_ENV.zip" -d "$ENV_DIR" || exit 3
+    wget "https://github.com/prusa3d/MM-build-env/releases/download/$BUILD_ENV/MM-build-env-$SYSTEM-$BUILD_ENV.zip" || exit 2
+    unzip "MM-build-env-$SYSTEM-$BUILD_ENV.zip" -d "$ENV_DIR" || exit 3
+    rm "MM-build-env-$SYSTEM-$BUILD_ENV.zip"
 fi
 
 export PATH=$SCRIPT_PATH/$ENV_DIR/avr/bin:$PATH
